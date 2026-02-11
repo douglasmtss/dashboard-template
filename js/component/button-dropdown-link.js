f7743de98727fe7d7c364b58b7fa1d7725a92154
douglasmtss <douglasmatosdev@gmail.com>
2026-02-11 07:45:34 -0300

    chore:remove jquery.animateAuto

--- js/component/button-dropdown-link.js
+++ js/component/button-dropdown-link.js
@@ -2,23 +2,6 @@ import { generateElementId } from "../util/generate-element-id.js";
 import { ChevronIcon } from "./chevron-icon.js";
 import { DropdownComponent } from './dropdown.js';
 
-jQuery.fn.animateAuto = function (prop, speed, callback) {
-    var elem, height, width;
-    return this.each(function (i, el) {
-        el = jQuery(el), elem = el.clone().css({ "height": "auto", "width": "auto" }).appendTo("body");
-        height = elem.css("height"),
-            width = elem.css("width"),
-            elem.remove();
-
-        if (prop === "height")
-            el.animate({ "height": height }, speed, callback);
-        else if (prop === "width")
-            el.animate({ "width": width }, speed, callback);
-        else if (prop === "both")
-            el.animate({ "width": width, "height": height }, speed, callback);
-    });
-}
-
 /**
  * @param {import('../type/types.js').ButtonDropdownLinkProps} props - properties for button-dropdown-link component
  * @returns {string} HTML string for button-dropdown-link component
