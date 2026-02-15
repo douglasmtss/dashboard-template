b48e9573c12bb8bec154407e7f9ddebbdcfa037a
douglasmtss <douglasmatosdev@gmail.com>
2026-02-15 17:06:11 -0300

    Remove unused chart, UI components, and type definitions from the project

--- js/footer.js
+++
@@ -1,8 +0,0 @@
-// footer.js
-export function renderFooter(container) {
-  container.innerHTML = `
-    <footer class="bg-light text-center py-3 mt-4">
-      <small>&copy; 2026 Prêmio Pintor ABRAPP Jornal do Pintor</small>
-    </footer>
-  `;
-}
