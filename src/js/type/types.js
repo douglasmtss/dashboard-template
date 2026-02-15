12f4052f966c1338026e476b4d6d032ea060900b
douglasmtss <douglasmatosdev@gmail.com>
2026-02-15 17:05:50 -0300

    Add type definitions for dropdown items and sidebar configuration

---
+++ src/js/type/types.js
@@ -0,0 +1,26 @@
+/**
+ * @typedef {Object} DropdownItemPropTypes
+ * @property {string} value
+ * @property {string} title
+ * @property {function} onclick
+ * @property {string} href
+ * @property {DropdownItemPropTypes[]} dropdown
+ */
+
+/**
+ * @typedef {Object} ButtonDropdownLinkProps
+ * @property {string} value - button text
+ * @property {string} href - button link
+ * @property {function} onclick - click event handler
+ * @property {DropdownItemPropTypes[]} dropdown - array of dropdown items
+ */
+
+/**
+ * @typedef {Object} SidebarConfigPropTypes
+ * @property {string} img - URL da imagem do logo
+ * @property {string} title - título da sidebar
+ * @property {Array<ButtonDropdownLinkProps>} items - array de itens de menu
+ */
+
+// Mark the file as a module for type compatibility
+export {};
